213e55ffd57fc915bddd4afdd0e5da33a88474df
Odyssey346 <odyssey346@disroot.org>
2023-02-14 14:46:24 +0100

    Fix bio
    Signed-off-by: Odyssey346 <odyssey346@disroot.org>

--- src/lib/components/Bio.svelte
+++ src/lib/components/Bio.svelte
@@ -4,7 +4,7 @@
     export let noReadMore: boolean = false;
 </script>
 
-<div class="flex flex-col items-left gap-4 mt-5 max-w-xl">
+<div class="flex flex-col items-left gap-4 mt-5 max-w-xl w-full">
     <h2 class="text-2xl">About</h2>
     <p>I am a 15 year old developer and sysadmin from Norway.</p>
     <p>I enjoy metal/rock music, sometimes taking pictures and looking at cats.</p>
