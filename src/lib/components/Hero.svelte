a9c05cc243b1c89373bcbda045bccf4989e0f203
Odyssey346 <odyssey346@disroot.org>
2023-02-27 21:31:24 +0100

    Add Directus to tech icons
    Signed-off-by: Odyssey346 <odyssey346@disroot.org>

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -8,10 +8,11 @@
     <div class="flex flex-row gap-4 text-3xl">
         <div class="i-simple-icons:git hover:text-[#F05032] transition-all" title="Git"></div>
         <div class="i-simple-icons:svelte hover:text-[#FF3E00] transition-all" title="Svelte"></div>
+        <div class="i-simple-icons:directus hover:text-[#8866ff] transition-all" title="Directus"></div>
         <div class="i-simple-icons:go hover:text-[#00ADD8] transition-all" title="Go"></div>
-        <div class="i-simple-icons:docker hover:text-[#2496ED] transition-all " title="Docker"></div>
+        <div class="i-simple-icons:docker hover:text-[#2496ED] transition-all" title="Docker"></div>
         <div class="i-file-icons:caddy hover:text-[#22b638] transition-all" title="Caddy"></div>
-        <div class="i-simple-icons:hugo hover:text-[#FF4088] transition-all " title="Hugo"></div>
+        <div class="i-simple-icons:hugo hover:text-[#FF4088] transition-all" title="Hugo"></div>
         <div class="i-simple-icons:unocss hover:text-[#333333] transition-all" title="UnoCSS"></div>
     </div>
 </div>
