d16dc6652d87c699d328df14f216b9bb4341097a
Odyssey346 <odyssey346@disroot.org>
2023-04-05 11:30:34 +0200

    Fresh up design a little
    Signed-off-by: Odyssey346 <odyssey346@disroot.org>

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -30,13 +30,19 @@
     <div class="flex flex-col items-left gap-4 mt-5 max-w-xl">
         <h2 class="text-2xl" id="donate">Donate</h2>
         <p>I can accept donations in Monero or Bitcoin.</p>
-        <h3 class="text-3xl" id="monero">Monero</h3>
+        <h3 class="text-xl" id="monero">Monero</h3>
         <p>My Monero address is: <code class="break-all bg-thirdbackground p-0.5 rounded-md">45wYu3oEAhu7s9aodftL2iBztYCdKzTZ4gaTRXGRRsN8cfVbdDMrVCEDmUczBiosE3ESVtyRKodXKgcTThyNsF2XGJvee5V</code></p>
         <p>You can also put in odyssey346.dev in your Monero client if it supports OpenAlias.</p>
-        <img src="/ha4t207b.bmp" alt="Monero donation QR code" class="w-1/2 rounded-xl" />
-        <h3 class="text-3xl" id="bitcoin">Bitcoin</h3>
+        <details title="QR code" class="cursor-pointer">
+            <summary>QR code</summary>
+            <img src="/ha4t207b.bmp" alt="Monero donation QR code" class="w-1/2 rounded-xl" />
+        </details>
+        <h3 class="text-xl" id="bitcoin">Bitcoin</h3>
         <p>My Bitcoin address is: <code class="break-all bg-thirdbackground p-0.5 rounded-md">bc1qsk2ntfzvl4zfkrfkmqp65w0q9xn9v09xvfezz7</code></p>
-        <img src="/bitcoin.png" alt="Bitcoin donation QR code" class="w-1/2 rounded-xl" />
+        <details title="QR code" class="cursor-pointer">
+            <summary>QR code</summary>
+            <img src="/bitcoin.png" alt="Bitcoin donation QR code" class="w-1/2 rounded-xl" />
+        </details>
         <p class="text-xl">Thank you for your support!</p>
         <ReadMoreButton href="/" text="Want to go back home?" />
     </div>
